88cda5c240bf1077ab8b1271ad40ef9f7862b777
Austin <austinblackman13@gmail.com>
2020-02-04 20:52:12 -0500

    added comments

--- open_programs.py
+++ open_programs.py
@@ -1,7 +1,9 @@
+# NOTE PATHS TO PROGRAMS WILL DIFFER PER USER
+
 import os, subprocess
 
-options = ['Development kit', 'Gaming kit']
 counter = 1
+options = ['Development kit', 'Gaming kit'] # Available kits to open
 
 print('What would you like to open:')
 
@@ -10,19 +12,20 @@ for option in options: # Prints all available options
     counter += 1
 
 prompt_user = '--> '
-acceptable = False
+acceptable_input = False
 
-while not acceptable:
+while not acceptable_input:
     user_input = input(prompt_user)
-    if user_input == "1":
+    if user_input == "1": # Open spotify and firefox with stack overflow
+                          # PATHS TO PROGRAMS WILL DIFFER PER USER
         print('Opening {}'.format(options[0]))
         subprocess.Popen([r'C:\Users\Austin\AppData\Roaming\Spotify\Spotify.exe']) 
         subprocess.Popen([r'C:\Program Files\Mozilla Firefox\firefox.exe', '-new-tab', 'https://stackoverflow.com/'])
-        acceptable = True
+        acceptable_input = True
     elif user_input == "2":
-        print('Opening {}'.format(options[1]))
+        print('Opening {}'.format(options[1])) # Open spotify and steam
         subprocess.Popen([r'C:\Users\Austin\AppData\Roaming\Spotify\Spotify.exe', r'C:\Program Files (x86)\Steam\Steam.exe'])
         subprocess.Popen([r'C:\Program Files (x86)\Steam\Steam.exe'])    
-        acceptable = True
+        acceptable_input = True
     else:
         print('You did not enter a valid option')
